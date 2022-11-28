d50b7b6674ae50e5c522d43a7a322c6d3022a429
Yu Wang <farheart@gmail.com>
2022-11-28 14:03:34 -0800

    Clean GreedySolver

--- src/main/java/wy/alns/algrithm/solver/GreedySolver.java
+++ src/main/java/wy/alns/algrithm/solver/GreedySolver.java
@@ -41,45 +41,47 @@ public class GreedySolver {
         Route curRoute = this.routeList.remove(0);
 
         // Add the depot as the 1st stop of the route
-        Delivery depot = this.instance.getDepot();
-
-        curRoute.append(depot);
+        curRoute.append(this.instance.getDepot());
 
         // Loop until all delivery are batched or no available vehicles.
-        while ((this.instance.getDeliveryList().size() > 0) && (this.routeList.size() > 0)) {
+        while (this.instance.getDeliveryList().size() > 0) {
 
             // Find the nearest delivery to the last node, which also meets all constraints
-            Delivery lastDelivery = curRoute.getLastNodeOfTheRoute();
-            Delivery nearestDelivery = findNearestDelivery(curRoute, lastDelivery);
+            Delivery nearestDelivery = findNearestDelivery(curRoute);
 
             // Find a node meeting the capacity constraint
             if (nearestDelivery != null) {
-                this.addToRoute(curRoute, lastDelivery, nearestDelivery);
+                this.addToRoute(curRoute, nearestDelivery);
 
                 // Remove delivery from the non-served list.
                 this.instance.getDeliveryList().remove(nearestDelivery);
 
             } else {
                 // Close the route and add to solution
-                this.closeRoute(curRoute, lastDelivery, depot);
+                this.closeRoute(curRoute);
                 this.addRoute(solution, curRoute);
 
                 // Create a new route if still have available vehicle
                 if (this.routeList.size() > 0) {
                     curRoute = this.routeList.remove(0);
-                    curRoute.append(depot);
+                    curRoute.append(this.instance.getDepot());
+                } else {
+                    // no vehicles, break loop
+                    break;
                 }
             }
         }
 
-        // Add the final route to solution
-        this.closeRoute(curRoute, curRoute.getLastNodeOfTheRoute(), depot);
+        // Close and add the final route to solution
+        this.closeRoute(curRoute);
         this.addRoute(solution, curRoute);
 
         return solution;
     }
 
-    private void addToRoute(Route curRoute, Delivery lastDelivery, Delivery newDelivery) {
+    private void addToRoute(Route curRoute, Delivery newDelivery) {
+        Delivery lastDelivery = curRoute.getLastNodeOfTheRoute();
+
         double d = this.instance.getDistanceDict().between(lastDelivery, newDelivery);
 
         // Increase total distance of the route by adding the distance of previous last node to the new one
@@ -102,13 +104,18 @@ public class GreedySolver {
         curRoute.append(newDelivery);
     }
 
+
     private static void addRoute(Solution solution, Route curRoute) {
         // Add route to the solution
         solution.addRoute(curRoute);
         solution.setTotalCost(solution.getTotalCost() + curRoute.getMeasure().distance);
     }
 
-    private void closeRoute(Route curRoute, Delivery lastDelivery, Delivery depot) {
+
+    private void closeRoute(Route curRoute) {
+        Delivery lastDelivery = curRoute.getLastNodeOfTheRoute();
+        Delivery depot = this.instance.getDepot();
+
         // Send back to depot to close the trip
         curRoute.getMeasure().distance += this.instance.getDistanceDict().between(lastDelivery, depot);
         curRoute.getMeasure().time += this.instance.getDistanceDict().between(lastDelivery, depot);
@@ -116,7 +123,10 @@ public class GreedySolver {
         curRoute.append(depot);
     }
 
-    private Delivery findNearestDelivery(Route curRoute, Delivery lastDelivery) {
+
+    private Delivery findNearestDelivery(Route curRoute) {
+        Delivery lastDelivery = curRoute.getLastNodeOfTheRoute();
+
         // 1st stop is the depot
         Delivery depot = curRoute.getDeliveryList().get(0);
 
